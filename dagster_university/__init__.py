0a82ca073849306a7c5e781c241c331ea90104b1
Tim Castillo <tim@elementl.com>
2023-06-21 15:46:41 -0400

    creating a barebones project for the scaffold

--- dagster_university/__init__.py
+++ dagster_university/__init__.py
@@ -1,35 +1,9 @@
 from dagster import Definitions, load_assets_from_modules
 
-import os
+from .assets import trips
 
-from .assets import trips, metrics, requests, dbt_assets
-from .resources import database_resource, dbt_resource
-from .jobs import trip_update_job, weekly_update_job, adhoc_request_job
-from .schedules import trip_update_schedule, weekly_update_schedule
-from .sensors import adhoc_request_sensor
-
-## Lesson 5 -> prob every other section after that
 trip_assets = load_assets_from_modules([trips])
-metric_assets = load_assets_from_modules(
-    modules=[metrics],
-    group_name="metrics",
-)
-requests_assets = load_assets_from_modules(
-    modules=[requests],
-    group_name="requests",
-)
-
-all_jobs = [trip_update_job, weekly_update_job, adhoc_request_job]
-all_schedules = [trip_update_schedule, weekly_update_schedule]
-all_sensors = [adhoc_request_sensor]
 
 defs = Definitions(
-    assets=[*trip_assets, *metric_assets, *requests_assets], # dbt_assets.transformations],
-    resources={
-        "database": database_resource,
-        "dbt": dbt_resource,
-    },
-    jobs=all_jobs,
-    schedules=all_schedules,
-    sensors=all_sensors,
+    assets=trip_assets
 )
