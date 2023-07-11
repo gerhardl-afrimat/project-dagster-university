5dc8299d0fc9c05ccb6dd0320ec2e34fccb655b9
Erin Cochran <erin.k.cochran@gmail.com>
2023-07-11 12:03:54 -0400

    Add taxi_zones_file asset

--- dagster_university/assets/trips.py
+++ dagster_university/assets/trips.py
@@ -17,3 +17,16 @@ def taxi_trips_file():
         constants.TAXI_TRIPS_TEMPLATE_FILE_PATH.format(month_to_fetch), "wb"
     ) as output_file:
         output_file.write(raw_trips.content)
+
+
+@asset
+def taxi_zones_file():
+    """
+    The raw CSV file for the taxi zones dataset. Sourced from the NYC Open Data portal.
+    """
+    raw_taxi_zones = requests.get(
+        "https://data.cityofnewyork.us/api/views/755u-8jsi/rows.csv?accessType=DOWNLOAD"
+    )
+
+    with open("data/raw/taxi_zones.csv", "wb") as output_file:
+        output_file.write(raw_taxi_zones.content)
